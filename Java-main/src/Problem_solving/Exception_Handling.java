554e2d67fd63905113c90d1918737bce3f0160f9
Vikrant-Singh <128889858+Luciesprogram@users.noreply.github.com>
2025-04-11 10:10:28 +0530

    Add files via upload

---
+++ Java-main/src/Problem_solving/Exception_Handling.java
@@ -0,0 +1,20 @@
+//package Problem_solving;
+//
+//public class Exception_Handling {
+//    public static void main(String[] args) {
+//        // String out of exception handling
+//        //String name = "Vikrant";
+//        //System.out.println("My Name: "+name.charAt(10));
+//
+//
+//        // NullPointerExeception==4
+//        String a = null;
+//        System.out.println("My name: "+a+a.length());
+//
+//        try {
+//            String a = null;
+//            System.out.println("My Name: "+a.length());
+//
+//        }
+//    }
+//}
